d48f11e51224d9b1394b4903cc563b46d81fd1ec
Abrar Habib <abrarhabib285@gmail.com>
2023-04-27 08:09:35 -0400

    refactored code

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,24 @@
 use std::error::Error;
+use std::fs;
 use std::path::Path;
-use image::GenericImageView;
 use image::io::Reader as ImageReader;
 use image::imageops::FilterType;
 
 pub struct Config {
-    pub file_path: String,
+    pub input_path: String,
+    pub output_path: String,
 }
 
 impl Config {
     pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 2 {
-            return Err("Not enough arguments provided!\nUsage: ascii-rs [file_path]");
+        if args.len() < 3 {
+            return Err("Not enough arguments provided!\nUsage: ascii-rs [image_path] [output_path]");
         }
-        let file_path = args[1].clone();
-        println!("{} opened.",&file_path);
+        let input_path = args[1].clone();
+        let output_path = args[2].clone();
         Ok(Config{
-            file_path
+            input_path,
+            output_path,
         })
     }
 }
@@ -26,20 +28,31 @@ fn map_range(val: i32, from: (i32, i32), to: (i32, i32)) -> i32 {
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let output_path = Path::new(&config.output_path);
+    if output_path.extension().unwrap() != "txt" {
+        return Err("Output file must be a .txt file.".into());
+    }
+    
     let density = vec!["Ñ","@","#","W", "$", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", "?", "!", "a", "b", "c", ";", ":", "+", "=", "-", ",", ".", "_", " ", " ", " ", " "," "," "," "," "," "];
-    ImageReader::open(config.file_path).expect("Image not found.").decode()?.resize(128, 128, FilterType::Gaussian).save(Path::new("ferris.png")).unwrap();    
-    let gray_image = ImageReader::open("ferris.png").expect("Image not found.").decode()?.grayscale().clone().into_luma8();
+    let gray_image = ImageReader::open(&config.input_path).expect("Image not found.").decode()?.resize(512, 512, FilterType::Gaussian).grayscale().clone().into_luma8();    
+    println!("Opened {}.",&config.input_path);
 
     let (width, height) = (gray_image.width(), gray_image.height());
+    let mut output = String::new();
     for column in 0..height {
         for row in 0..width {
-        // let mut output = String::new();
             let pixel = gray_image.get_pixel(row, column);
             let luma = pixel[0];
             let index = map_range(luma as i32, (0, 255), (density.len() as i32 - 1, 0));
-            print!("{}", density[index as usize]);
+            output += &format!("{}", density[index as usize]);
         }
-        println!();
+        output += "\n";
     }
+
+    
+
+    fs::write(&config.output_path, output)?;
+    println!("Saved ascii drawing to {}.", &config.output_path);
+
     Ok(())
 }
